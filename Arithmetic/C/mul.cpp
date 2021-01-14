25477cb5a3c2e2730547cc89e47185d735e0c366
wwj <1273896501@qq.com>
2021-01-14 19:56:04 +0800

    增加测试用例并使用INI文件

--- Arithmetic/C/mul.cpp
+++ Arithmetic/C/mul.cpp
@@ -3,5 +3,13 @@
 
 int mul(int multiplied, int multiplier)
 {
-	return multiplied * multiplier;
+	long long a, b, mul;
+	a = (long long)multiplied;
+	b = (long long)multiplier;
+	mul = a*b;
+	if (mul <= 2147483647 && mul >= -(long long)2147483648)
+	{
+		return (int)mul;
+	}
+	return 0;
 }
--- Arithmetic/UnitTestC/unittestC.cpp
+++ Arithmetic/UnitTestC/unittestC.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include"../C/mul.h"
+#include"Windows.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,9 +13,16 @@ namespace UnitTestC
 		
 		TEST_METHOD(Testmul)
 		{
-			int expect = 2;
-			int output = mul(2, 1);
-			Assert::IsTrue(expect == output);
+			int expect, output, multiplied, multiplier, i;
+			WCHAR* pmul[6] = { L"mul1", L"mul2", L"mul3", L"mul4", L"mul5", L"mul6", };
+			for (i = 0; i < 6; i++)
+			{
+				expect = GetPrivateProfileInt(pmul[i], L"expect", NULL, L"../²âÊÔÓÃÀý.ini");
+				multiplied = GetPrivateProfileInt(pmul[i], L"multiplied", NULL, L"../²âÊÔÓÃÀý.ini");
+				multiplier = GetPrivateProfileInt(pmul[i], L"multiplier", 1, L"../²âÊÔÓÃÀý.ini");
+				output = mul(multiplied, multiplier);
+				Assert::IsTrue(expect == output);
+			}
 		}
 
 	};
