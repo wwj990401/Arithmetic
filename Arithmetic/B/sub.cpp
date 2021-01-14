d5ee6f828a9daa49784dab5e61bce5ccc392311f
wwj <1273896501@qq.com>
2021-01-14 19:30:44 +0800

    增加测试用例并使用INI文件

--- Arithmetic/B/sub.cpp
+++ Arithmetic/B/sub.cpp
@@ -1,7 +1,16 @@
 #include<stdio.h>
 #include"sub.h"
+#include"assert.h"
 
 int sub(int subtracted, int subtract)
 {
-	return subtracted - subtract;
+	long long a, b, sub;
+	a = (long long)subtracted;
+	b = (long long)subtract;
+	sub = a - b;
+	if (sub <= 2147483647 && sub >= -(long long)2147483648)
+	{
+		return (int)sub;
+	}
+	return 0;
 }
--- Arithmetic/UnitTestB/unittestB.cpp
+++ Arithmetic/UnitTestB/unittestB.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include"../B/sub.h"
+#include <Windows.h>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,9 +13,16 @@ namespace UnitTestB
 		
 		TEST_METHOD(Testsub)
 		{
-			int expect = 2;
-			int output = sub(3, 1);
-			Assert::IsTrue(expect == output);
+			int expect, output,subtracted, subtract, i;
+			WCHAR* psub[6] = { L"sub1", L"sub2", L"sub3", L"sub4", L"sub5", L"sub6", };
+			for (i = 0; i < 6; i++)
+			{
+				expect = GetPrivateProfileInt(psub[i], L"expect", NULL, L"../²âÊÔÓÃÀý.ini");
+				subtracted = GetPrivateProfileInt(psub[i], L"subtracted", NULL, L"../²âÊÔÓÃÀý.ini");
+				subtract = GetPrivateProfileInt(psub[i], L"subtract", 1, L"../²âÊÔÓÃÀý.ini");
+				output = sub(subtracted, subtract);
+				Assert::IsTrue(expect == output);
+			}
 		}
 
 	};
