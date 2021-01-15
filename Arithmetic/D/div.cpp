4f941cc0f3aaac8a1b87ea76590f4743ff8b4a21
wwj <1273896501@qq.com>
2021-01-15 10:18:51 +0800

    增加测试用例并使用INI文件

--- Arithmetic/D/div.cpp
+++ Arithmetic/D/div.cpp
@@ -1,10 +1,42 @@
 #include<stdio.h>
 #include"div.h"
 
-div_t div1(int a, int b)
+div_t div1(int numer, int denom)
 {
 	div_t output;
-	output.quot = a / b;
-	output.rem = a%b;
+	if (denom == 0)
+	{
+		output.quot = 0;
+		output.rem = 0;
+		return output;
+	}
+	if (numer == -2147483648 && denom == -1)
+	{
+		output.quot = 0;
+		output.rem = 0;
+		return output;
+	}
+	if ((numer%denom) != 0)
+	{
+		if ((numer<0 && denom>0) || (numer > 0 && denom < 0))
+		{
+			output.quot = (numer / denom) - 1;
+			output.rem = (numer%denom) + denom;
+			return output;
+		}
+	}
+	if (numer == -2147483648 && denom < -1)
+	{
+		long long Lnumer, Ldenom, quot, rem;
+		Lnumer = -(long long)numer;
+		Ldenom = -(long long)denom;
+		quot = Lnumer / Ldenom;
+		rem = -Lnumer%Ldenom;
+		output.quot = (int)quot;
+		output.rem = (int)rem;
+		return output;
+	}
+	output.quot = numer / denom;
+	output.rem = numer%denom;
 	return output;
 }
--- Arithmetic/UnitTestD/unittestD.cpp
+++ Arithmetic/UnitTestD/unittestD.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include"../D/div.h"
+#include"Windows.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -12,10 +13,21 @@ namespace UnitTestD
 		
 		TEST_METHOD(Testdiv)
 		{
-			div_t expect = { 1, 2 };
-			div_t output = div1(5, 3);
-			Assert::IsTrue(expect.quot == output.quot);
-			Assert::IsTrue(expect.rem == output.rem);
+			div_t expect, output;
+			int expect_quot, expect_rem, numer, denom, i;
+			WCHAR* pdiv[11] = { L"div1", L"div2", L"div3", L"div4", L"div5", L"div6", L"div7", L"div8", L"div9", L"div10", L"div11"};
+			for (i = 0; i < 11; i++)
+			{
+				expect_quot = GetPrivateProfileInt(pdiv[i], L"expect_quot", NULL, L"../²âÊÔÓÃÀý.ini");
+				expect_rem = GetPrivateProfileInt(pdiv[i], L"expect_rem", NULL, L"../²âÊÔÓÃÀý.ini");
+				expect.quot = expect_quot;
+				expect.rem = expect_rem;
+				numer = GetPrivateProfileInt(pdiv[i], L"numer", NULL, L"../²âÊÔÓÃÀý.ini");
+				denom = GetPrivateProfileInt(pdiv[i], L"denom", NULL, L"../²âÊÔÓÃÀý.ini");
+				output = div1(numer, denom);
+				Assert::IsTrue(expect.quot == output.quot);
+				Assert::IsTrue(expect.rem == output.rem);
+			}
 		}
 
 	};
