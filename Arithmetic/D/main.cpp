e84b9f2818350e890036634fabe85a7ec3633e7a
wwj <1273896501@qq.com>
2021-01-15 16:39:43 +0800

    修改说明文档并在mian调用四则运算

--- Arithmetic/D/main.cpp
+++ Arithmetic/D/main.cpp
@@ -10,20 +10,32 @@
 #include <Windows.h>
 
 
-typedef int(*PMUL)(int a, int b);
+typedef int(*PMUL)(int a, int b, int* result);
 
 int main()
 {
+	int* pflag;
+	int* presultadd;
+	int* presultsub;
+	int* presultmul;
+	int flag;
+	int resultadd;
+	int resultsub;
+	int resultmul;
+	pflag = &flag;
+	presultadd = &resultadd;
+	presultsub = &resultsub;
+	presultmul = &resultmul;
 	int a, b;
 	div_t output;
 	printf("请输入两个数：\n");
 	scanf("%d%d", &a, &b);
-	output = div1(a, b);
-	printf("div1(%d/%d)=%d余%d\n", a, b, output.quot, output.rem);
+	output = div1(a, b, pflag);
+	printf("div1(%d/%d)=%d余%d\n异常情况：%d\n", a, b, output.quot, output.rem, flag);
 
-	printf("add(%d+%d)=%d\n", a, b, add(a, b));
+	printf("add(%d+%d)=%d\n异常情况：%d\n", a, b, resultadd, add(a, b, presultadd));
 
-	printf("sub(%d-%d)=%d\n", a, b, sub(a, b));
+	printf("sub(%d-%d)=%d\n异常情况：%d\n", a, b, resultsub, sub(a, b, presultsub));
 
 	HMODULE hDLL = LoadLibrary(L"C.dll");
 	if (hDLL == NULL)
@@ -32,7 +44,7 @@ int main()
 		return 0;
 	}
 	PMUL mul = (PMUL)GetProcAddress(hDLL, "mul");
-	printf("mul(%d*%d)=%d\n", a, b, mul(a, b));
+	printf("mul(%d*%d)=%d\n异常情况：%d\n", a, b, resultmul, mul(a, b, presultmul));
 	FreeLibrary(hDLL);
 	return 0;
 }
